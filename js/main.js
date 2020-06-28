7a110e8304b9c7402865fbd44a62998d57d6f4ae
Ezzdin Atef <ezzdin1125@gmail.com>
2020-06-29 00:06:36 +0200

    add function and files

--- js/main.js
+++ js/main.js
@@ -1,40 +1,59 @@
 const elements = {
-  container: document.querySelector('.container'),
+  container: document.querySelector(".container"),
+  left: document.querySelector(".container .left"),
+  center: document.querySelector(".container .center"),
+  right: document.querySelector(".container .right"),
 };
-let name;
 
-let xhr = new XMLHttpRequest();
+function getData(name, position) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", `json/${name}.json`, true);
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      let theResponse = JSON.parse(xhr.responseText);
+      // Create the main div
+      const mainDiv = document.createElement("div");
 
-// Potst
-name = 'posts'; // change this name
-xhr.open('GET', `json/${name}.json`, true);
-xhr.send();
-xhr.onreadystatechange = () => {
-  if (xhr.status === 200 && xhr.readyState === 4) {
-    let theResponse = JSON.parse(xhr.responseText);
-    // Create the main div
-    const mainDiv = document.createElement('div');
+      // create the header
+      const heading = document.createElement("h3");
+      heading.appendChild(document.createTextNode(name));
+      mainDiv.appendChild(heading);
 
-    // create the header
-    const heading = document.createElement('h3');
-    heading.appendChild(document.createTextNode(name));
-    mainDiv.appendChild(heading);
+      // create the funtion list ul
+      const ul = document.createElement("ul");
+      theResponse.forEach((element) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = element.link;
+        a.target = "_blank";
 
-    // create the funtion list ul
-    const ul = document.createElement('ul');
-    theResponse.forEach(element => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = element.link;
-      a.target = '_blank';
-      a.appendChild(document.createTextNode(element.name));
-      li.appendChild(a);
-      ul.appendChild(li);
-    });
-    mainDiv.appendChild(ul);
+        const code = document.createElement("code");
+        code.appendChild(document.createTextNode(element.name));
 
+        a.appendChild(code);
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+      mainDiv.appendChild(ul);
 
-    // append the element to the container
-    elements.container.appendChild(mainDiv);
-  }
-};
+      // append the element to the container
+      if (position === "right") elements.right.appendChild(mainDiv);
+      else if (position === "left") elements.left.appendChild(mainDiv);
+      else if (position === "center") elements.center.appendChild(mainDiv);
+    }
+  };
+}
+
+// left
+getData("posts", "left");
+getData("author", "left");
+getData("comments", "left");
+
+// center
+getData("essentials", "center");
+
+// right
+
+getData("categories", "right");
+getData("sidebar", "right");
